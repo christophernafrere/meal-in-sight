246ecbd4fd458e69a50679991ecb5608680ddd0a
christophernafrere <nafrere.christopher@gmail.com>
2025-10-24 18:58:11 +0200

    first commit

---
+++ apps/web/src/app/global-styles.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`;
+export default GlobalStyles;

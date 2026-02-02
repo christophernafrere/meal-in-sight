591c355d29cfe4627afbe08f467a94a6e4a2dafd
christophernafrere <nafrere.christopher@gmail.com>
2026-02-02 10:33:34 +0100

    feat: add icons and manifest for PWA support

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -24,14 +24,29 @@ export const metadata: Metadata = {
     icons: {
         icon: [
             {
-                url: '/icon.png',
+                url: '/favicon-16x16.png',
+                type: 'image/png',
+                sizes: '16x16',
+            },
+            {
+                url: '/favicon-32x32.png',
+                type: 'image/png',
+                sizes: '32x32',
+            },
+            {
+                url: '/android-chrome-192x192.png',
+                type: 'image/png',
+                sizes: '192x192',
+            },
+            {
+                url: '/android-chrome-512x512.png',
                 type: 'image/png',
                 sizes: '512x512',
             },
         ],
         apple: [
             {
-                url: '/icon.png',
+                url: '/apple-touch-icon.png',
                 type: 'image/png',
                 sizes: '180x180',
             },
